c852e1ad0f2edaebbf2838e9b7752b01ea9b122b
elisirdina <elisirdina.idr@gmail.com>
2024-12-19 11:43:59 +0800

    Update main.js

--- docs/main.js
+++ docs/main.js
@@ -37,14 +37,13 @@ window.addEventListener('scroll', () => {
 async function fetchData() {
     try {
         const apiUrl = 'https://infobencanajkmv2.jkm.gov.my/api/data-dashboard-table-pps.php?a=0&b=0&seasonmain_id=208&seasonnegeri_id=';
-        const proxyUrl = 'http://localhost:3000/proxy?url='; // Use the local proxy server
+        const proxyUrl = 'https://cors-anywhere.herokuapp.com/'; // CORS Anywhere service
 
-        const response = await fetch(proxyUrl + encodeURIComponent(apiUrl), {
+        const response = await fetch(proxyUrl + apiUrl, {
             headers: {
                 'Accept': 'application/json'
             }
         });
-
 
         if (!response.ok) {
             throw new Error(`HTTP error! status: ${response.status}`);
