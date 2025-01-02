98a0e2b233e16646432b85b1e21166b781d1d5c4
elisirdina <elisirdina.idr@gmail.com>
2025-01-02 14:52:04 +0800

    Update main.js

--- docs/main.js
+++ docs/main.js
@@ -370,7 +370,7 @@ async function fetchGeoJsonData(url) {
 // Fetch PPS data from the API
 async function fetchPpsData() {
     try {
-        const proxyUrl = 'https://corsproxy.io/?';
+        const proxyUrl = 'https://api.allorigins.win/get?url=';
         const apiUrl = 'https://infobencanajkmv2.jkm.gov.my/api/pusat-buka.php?a=0&b=0';
         const response = await fetch(proxyUrl + apiUrl, {
             headers: {
